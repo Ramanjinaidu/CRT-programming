b8c6bd84d9327f907873031ae24d7138dd58d06d
Ramanji Naidu <kathiramanji1@gmail.com>
2026-01-30 09:12:30 +0530

    digits problems

--- IV-SEM/Python/Practice/M02_Logic_Building_and_patterns/PS01_Digits_Problems.py
+++ IV-SEM/Python/Practice/M02_Logic_Building_and_patterns/PS01_Digits_Problems.py
@@ -39,11 +39,62 @@
 # print("Even_Sum:",ev_sum)
 # print("Odd_Sum:",od_sum)
 
-#4. add digits of a number until the sum becomes single digit.
+#1 Count the number of digits in a given number.
+"""
+n = int(input())
+print(len(str(n)))
+
+or
 
 n = int(input())
-while n > 10:
-    n = sum(list(map(int,str(n))))
-print(n)
+count = 0
+for i in str(n):
+    count += 1
+print(count)
+"""
+#2 Finding sum of digits in a number
+"""
+n = int(input())
+s = 0
+for i in str(n):
+    digit = n%10
+    s += digit
+    n = n//10
+print(s)
 
-            
+# 3 Reverse the number
+n = int(input())
+rev = 0
+while n>0:
+    digit = n%10
+    rev = rev*10 + digit
+    n = n//10
+print(rev)
+"""
+# 4 check palindrome number
+"""
+n = int(input())
+rev = 0
+temp = n
+while n>0:
+    digit = n%10
+    rev = rev*10 + digit
+    n = n//10
+if rev == temp:
+    print("Palindrome")
+else:
+    print("Not a palindrome")
+"""
+# 5 check Armstrong number
+num = int(input())
+temp = num
+s = 0
+n = len(str(num))
+while num>0:
+    digit = num%10
+    s += digit**n
+    temp = temp//10
+if num == s:
+    print("Armstrong")
+else:
+    print("Not an Armstrong")
